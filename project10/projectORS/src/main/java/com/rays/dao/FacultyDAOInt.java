1021c9d104215152eddd11fdecc4e84043bb11e1
Nehaishu24 <nehaom24@gmal.com>
2024-06-13 12:55:12 +0530

    Done

---
+++ project10/projectORS/src/main/java/com/rays/dao/FacultyDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.FacultyDTO;
+/**
+ * @author Aakash Rathod
+ *
+ */
+public interface FacultyDAOInt extends BaseDAOInt<FacultyDTO> {
+
+}
---
+++ project10/projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * @author Aakash Rathod
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
